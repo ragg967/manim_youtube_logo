e7118937fcd6ea111f30794b2b1b0aa03fb58181
Riley Watson <rywatson1027@gmail.com>
2025-07-30 21:17:43 -0700

    baller

--- src/main.py
+++ src/main.py
@@ -10,7 +10,22 @@ from manim.utils.color.XKCD import ORANGEPINK
 
 class YoutubeLogo(Scene):
     def construct(self):
-        self.camera.background_color = ORANGEPINK  # type: ignore
+        # Dynamic background color transition
+        self.camera.background_color = BLACK  # type: ignore
+
+        # make reuseable global variables
+        colors = [ORANGEPINK, RED, PINK]
+
+        # Create a gradient background effect
+        gradient_bg = Rectangle(
+            width=config.frame_width,
+            height=config.frame_height,
+            fill_color=colors,
+            fill_opacity=1,
+            stroke_width=0,
+        )
+        self.add(gradient_bg)
+        self.play(FadeIn(gradient_bg), run_time=1)
 
         squigglyLine1 = (
             SVGMobject("assets/blog-line-pt.svg")
@@ -31,11 +46,32 @@ class YoutubeLogo(Scene):
             run_time=2,
         )
 
-        youtubeName = Text("Ragg", color=BLACK, font_size=72)
+        youtubeName = Text("Ragg", font="Lobster", color=BLACK, font_size=72)
+        text_shadow = (
+            youtubeName.copy()
+            .set_color_by_gradient(*colors)
+            .shift(0.1 * DOWN + 0.1 * RIGHT)
+        )
+        self.add(text_shadow)
         self.add(youtubeName)
-        self.play(DrawBorderThenFill(youtubeName), run_time=2)
-        self.play(youtubeName.animate.rotate(-10 * DEGREES), run_time=0.5)
+        self.play(
+            DrawBorderThenFill(text_shadow),
+            DrawBorderThenFill(youtubeName),
+            run_time=2,
+        )
+        self.play(
+            text_shadow.animate.rotate(-10 * DEGREES),
+            youtubeName.animate.rotate(-10 * DEGREES),
+            run_time=0.5,
+        )
 
-        self.play(FadeOut(squigglyLine1, squigglyLine2, youtubeName))
+        self.play(
+            FadeOut(
+                squigglyLine1,
+                squigglyLine2,
+                text_shadow,
+                youtubeName,
+            )
+        )
 
         self.wait(1)
